f2a354f9ddca6ce892ed5462bae89437e0de3c0d
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2023-03-14 01:01:04 +0000

    GET method to get user by id

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,21 @@ const users = [];
 router.post('/',(req,res) => {
     const user = req.body;
     users.push({...user, id: uuidv4() });
-    res.send(`User with first name ${req.body.firstName} added to database!!!`);
+    res.send(`User with first name ${user.firstName} added to database!!!`);
 });
 
 router.get('/', (req,res) => {
     res.send(users);
+});
+
+/**
+ * Get user by id
+ */
+router.get('/:id', (req,res) => {
+    console.log(req.params);
+    const {id} = req.params;
+    const userByID = users.find((user) => user.id === id);
+    res.send(userByID);
 });
 
 export default router;
