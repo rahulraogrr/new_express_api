a95a052b92a210c5819a95eba0ed777ed486bdc6
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2023-03-14 00:51:00 +0000

    POST method impl

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,15 @@
 import express from 'express';
+import {v4 as uuidv4} from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "Rahul Rao",
-        lastName: "Gonda",
-        city: "Paisley",
-        country: "United kingdom"
-    }
-];
+const users = [];
+
+router.post('/',(req,res) => {
+    const user = req.body;
+    users.push({...user, id: uuidv4() });
+    res.send(`User with first name ${req.body.firstName} added to database!!!`);
+});
 
 router.get('/', (req,res) => {
     res.send(users);
